a0599d4e376818a9af01046bed45abee655d0d5d
dbsghk320 <dbsghk320@gmail.com>
2022-04-28 15:04:11 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="top">
+    <a>어플리케이션 이름</a>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 b">
+        <button type="button" class="menu"> {{ menu[0] }} </button>
+      </div>
+      <div class="col-sm-6 b">
+        <button type="button" class="menu"> {{ menu[1] }} </button>
+      </div>
+      <div class="col-sm-6 b">
+        <button type="button" class="menu"> {{ menu[2] }} </button>
+      </div>
+      <div class="col-sm-6 b">
+        <button type="button" class="menu"> {{ menu[3] }} </button>
+      </div>
+    </div>
+  </div>
+  <div>
+    <!-- <h4 class="menu" v-for="작명 in menu" :key="작명"> {{ 작명 }} </h4> -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      menu : ['금융 상품 비교', '보이스 피싱 방지 어시스턴트',
+      '뱅킹 앱 자원봉사자 대면 도우미', '자원봉사자 신청'],
+      style : "color : blue",
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -22,5 +47,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.top {
+  background: darkblue;
+  padding: 15px;
+  border-radius : 5px;
+}
+.top a {
+  color : white;
+  padding: 10px;
+}
+
+.menu {
+  margin-left: auto;
+  margin-right: auto;
+  width : 40%;
+  height: 100%;
+  background: #363636;
+  border: #2c3e50 100px;
+  text-align: center;
 }
 </style>
